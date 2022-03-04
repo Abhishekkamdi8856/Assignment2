281ca809948785e96a4d062b53fd9040b6e2aa62
pc <pc@DESKTOP-GIBNGGQ>
2022-03-04 14:41:12 +0530

    Add project files.

---
+++ Abhi.cs
@@ -0,0 +1,60 @@
+﻿//using system;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+
+//namespace testconsoleapp
+//{
+//    class abhi
+//    {
+//        public static void main(string[] args)
+//        {
+//            add obj = new add();
+//            obj.addition();
+//            obj.substraction();
+//            obj.multiplication();
+//            obj.divide();
+//            console.readkey();
+
+
+//        }
+//    }
+//    class add
+//    {
+//        public void addition()
+//        {
+//            int a = 10;
+//            int b = 20;
+//            int res = a + b;
+//            console.writeline("the addition of a and b is " + res);
+
+
+//        }
+//        public void substraction()
+//        {
+//            int a = 15;
+//            int b = 5;
+//            int res = a - b;
+//            console.writeline("the substraction of a and b is " + res);
+
+
+//        }
+//        public void multiplication()
+//        {
+//            int a = 10;
+//            int b = 16;
+//            int res = a * b;
+//            console.writeline("the multiplication of a and b is " + res);
+//        }
+//        public void divide()
+//        {
+//            int a = 100;
+//            int b = 10;
+//            int res = a / b;
+//            console.writeline("the division of a and b is " + res);
+//        }
+//    }
+
+//}
---
+++ Class1.cs
@@ -0,0 +1,68 @@
+﻿//using System;
+//using LogicsP;
+//using LogicsP2;
+
+
+//namespace TestConsoleApp
+//{
+//    class Class1
+//    {
+//        public static void Main(String[] args)
+     
+//        {
+//            Add obj = new Add();
+//            obj.Addition();
+//            obj.Subsraction();
+//            obj.Multiplication();
+//            obj.Divide();
+          
+      
+//            Class2 obj1 = new Class2();
+//            obj1.DTImplimentation();
+
+//            Calculator obj2 = new Calculator();
+//            obj2.Add();
+//            obj2.Substraction();
+//            obj2.Multiplication();
+//            obj2.Division();
+
+
+//            Console.ReadKey();
+
+//        }
+
+
+//    }
+//  public  class Add
+//    {
+//        public void Addition()
+//        {
+//            int a = 10;
+//            int b = 100;
+//            int res = a + b;
+//            Console.WriteLine("Add is " + res);
+
+//        }
+//        public void Subsraction()
+//        {
+//            int a = 255;
+//            int b = 55;
+//            int res = a - b;
+//            Console.WriteLine("Substraction is " + res);
+//        }
+//        public void Multiplication()
+//        {
+//            int a = 20;
+//            int b = 10;
+//            int res = a * b;
+//            Console.WriteLine("Multipliaction is " + res);
+//        }
+//        public void Divide()
+//            {
+//            int a = 100;
+//            int b = 10;
+//            int res = a / b;
+//            Console.WriteLine("Division is " + res);
+//        }
+//    }
+//}
